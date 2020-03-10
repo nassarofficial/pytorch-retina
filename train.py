5159ed8505a34590c9fd22fdff52f13dc2c10843
Ahmed Nassar <nassarofficial@gmail.com>
2020-03-10 11:26:25 +0100

    train fixed

--- train.py
+++ train.py
@@ -10,10 +10,11 @@ from torchvision import transforms
 from retinanet import model
 from retinanet.dataloader import VOCDetection, collater, Resizer, AspectRatioBasedSampler, Augmenter, Normalizer
 from torch.utils.data import DataLoader
-
+from tensorboardX import SummaryWriter
 from retinanet import coco_eval
 from retinanet import csv_eval
 assert torch.__version__.split('.')[0] == '1'
+from tqdm.autonotebook import tqdm
 
 print('CUDA available: {}'.format(torch.cuda.is_available()))
 
@@ -30,9 +31,15 @@ def main(args=None):
                         type=str, help='Pasadena or mapillary')
     parser.add_argument("--overfit", type=int, default="0")
     parser.add_argument('--depth', help='Resnet depth, must be one of 18, 34, 50, 101, 152', type=int, default=50)
-    parser.add_argument('--epochs', help='Number of epochs', type=int, default=100)
+    parser.add_argument("--num_epochs", type=int, default=100)
+    parser.add_argument("--log_path", type=str, default="tensorboard/")
+    parser.add_argument("--test_interval", type=int, default=1, help="Number of epoches between testing phases")
+    parser.add_argument("--es_min_delta", type=float, default=0.0,
+                        help="Early stopping's parameter: minimum change loss to qualify as an improvement")
+    parser.add_argument("--es_patience", type=int, default=0,
+                        help="Early stopping's parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.")
 
-    parser = parser.parse_args(args)
+    opt = parser.parse_args(args)
     num_gpus = 1
     if torch.cuda.is_available():
         num_gpus = torch.cuda.device_count()
@@ -40,16 +47,16 @@ def main(args=None):
     else:
         torch.manual_seed(123)
 
-    if(parser.dataset == 'Pasadena' or parser.dataset == 'mapillary'):
-        train_dataset = VOCDetection(root=parser.dataset_root, overfit= parser.overfit, image_sets="trainval", transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]), dataset_name=parser.dataset)
-        valid_dataset = VOCDetection(root=parser.dataset_root, overfit= parser.overfit, image_sets="val", transform=transforms.Compose([Normalizer(), Resizer()]), dataset_name=parser.dataset)
+    if(opt.dataset == 'Pasadena' or opt.dataset == 'mapillary'):
+        train_dataset = VOCDetection(root=opt.dataset_root, overfit= opt.overfit, image_sets="trainval", transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]), dataset_name=opt.dataset)
+        valid_dataset = VOCDetection(root=opt.dataset_root, overfit= opt.overfit, image_sets="val", transform=transforms.Compose([Normalizer(), Resizer()]), dataset_name=opt.dataset)
 
     else:
         raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
     # sampler = AspectRatioBasedSampler(train_dataset, batch_size=2, drop_last=False)
 
-    training_params = {"batch_size": parser.batch_size * num_gpus,
+    training_params = {"batch_size": opt.batch_size * num_gpus,
                    "shuffle": True,
                    "drop_last": True,
                    "collate_fn": collater,
@@ -58,7 +65,7 @@ def main(args=None):
     training_generator = DataLoader(train_dataset, **training_params)
 
     if valid_dataset is not None:
-        test_params = {"batch_size": parser.batch_size,
+        test_params = {"batch_size": opt.batch_size,
                "shuffle": False,
                "drop_last": False,
                "collate_fn": collater,
@@ -67,15 +74,15 @@ def main(args=None):
         test_generator = DataLoader(valid_dataset, **test_params)
 
     # Create the model
-    if parser.depth == 18:
+    if opt.depth == 18:
         retinanet = model.resnet18(num_classes=train_dataset.num_classes(), pretrained=True)
-    elif parser.depth == 34:
+    elif opt.depth == 34:
         retinanet = model.resnet34(num_classes=train_dataset.num_classes(), pretrained=True)
-    elif parser.depth == 50:
+    elif opt.depth == 50:
         retinanet = model.resnet50(num_classes=train_dataset.num_classes(), pretrained=True)
-    elif parser.depth == 101:
+    elif opt.depth == 101:
         retinanet = model.resnet101(num_classes=train_dataset.num_classes(), pretrained=True)
-    elif parser.depth == 152:
+    elif opt.depth == 152:
         retinanet = model.resnet152(num_classes=train_dataset.num_classes(), pretrained=True)
     else:
         raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
@@ -92,7 +99,7 @@ def main(args=None):
         retinanet = torch.nn.DataParallel(retinanet)
 
     retinanet.training = True
-
+    writer = SummaryWriter(opt.log_path+"regigraph_bs_"+str(opt.batch_size)+"_dataset_"+opt.dataset+"_epoch_"+str(epoch+1)+"_backbone_"+str(opt.depth))
     optimizer = optim.Adam(retinanet.parameters(), lr=1e-5)
 
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
@@ -104,14 +111,18 @@ def main(args=None):
 
     print('Num training images: {}'.format(len(train_dataset)))
 
-    for epoch_num in range(parser.epochs):
+    num_iter_per_epoch = len(training_generator)
+
+    for epoch in range(opt.num_epochs):
 
         retinanet.train()
         retinanet.module.freeze_bn()
 
         epoch_loss = []
 
-        for iter_num, data in enumerate(training_generator):
+        progress_bar = tqdm(training_generator)
+
+        for iter, data in enumerate(progress_bar):
             try:
                 optimizer.zero_grad()
 
@@ -137,10 +148,16 @@ def main(args=None):
                 loss_hist.append(float(loss))
 
                 epoch_loss.append(float(loss))
+                total_loss = np.mean(epoch_loss)
 
-                print(
-                    'Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(
-                        epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist)))
+
+                progress_bar.set_description(
+                    'Epoch: {}/{}. Iteration: {}/{}. Cls loss: {:.5f}. Reg loss: {:.5f}. Batch loss: {:.5f} Total loss: {:.5f}'.format(
+                        epoch + 1, opt.num_epochs, iter + 1, num_iter_per_epoch, classification_loss, regression_loss, float(loss),
+                        total_loss))
+                writer.add_scalar('Train/Total_loss', total_loss, epoch * num_iter_per_epoch + iter)
+                writer.add_scalar('Train/Regression_loss', regression_loss, epoch * num_iter_per_epoch + iter)
+                writer.add_scalar('Train/Classfication_loss (focal loss)', classification_loss, epoch * num_iter_per_epoch + iter)
 
                 del classification_loss
                 del regression_loss
@@ -148,25 +165,56 @@ def main(args=None):
                 print(e)
                 continue
 
-        if parser.dataset == 'coco':
-
-            print('Evaluating dataset')
-
-            coco_eval.evaluate_coco(valid_dataset, retinanet)
-
-        else:
-
-            print('Evaluating dataset')
-
-            mAP = csv_eval.evaluate(valid_dataset, retinanet)
-
         scheduler.step(np.mean(epoch_loss))
 
-        torch.save(retinanet.module, 'trained_models/{}_retinanet_{}.pt'.format(parser.dataset, epoch_num))
+        if epoch % opt.test_interval == 0:
+            retinanet.eval()
+            loss_regression_ls = []
+            loss_classification_ls = []
+            for iter, data in enumerate(test_generator):
+                with torch.no_grad():
+                    if torch.cuda.is_available():
+                        classification_loss, regression_loss = retinanet([data['img'].cuda().float(), data['annot']])
+                    else:
+                        classification_loss, regression_loss = retinanet([data['img'].float(), data['annot']])
 
-    retinanet.eval()
+                    classification_loss = classification_loss.mean()
+                    regression_loss = regression_loss.mean()
 
-    torch.save(retinanet, 'trained_models/model_final.pt')
+                    loss_classification_ls.append(float(classification_loss))
+                    loss_regression_ls.append(float(regression_loss))
+
+            cls_loss = np.mean(loss_classification_ls)
+            reg_loss = np.mean(loss_regression_ls)
+            loss = classification_loss + regression_loss
+
+            print(
+                'Epoch: {}/{}. Classification loss: {:1.5f}. Regression loss: {:1.5f}. Total loss: {:1.5f}'.format(
+                    epoch + 1, opt.num_epochs, cls_loss, reg_loss,
+                    np.mean(loss)))
+            writer.add_scalar('Test/Total_loss', loss, epoch)
+            writer.add_scalar('Test/Regression_loss', reg_loss, epoch)
+            writer.add_scalar('Test/Classfication_loss (focal loss)', cls_loss, epoch)
+
+            if loss + opt.es_min_delta < best_loss:
+                best_loss = loss
+                best_epoch = epoch
+                mAP = csv_eval.evaluate(valid_dataset, retinanet)
+                print(mAP)
+                torch.save(model, os.path.join(opt.saved_path, "regigraph_bs_"+str(opt.batch_size)+"_dataset_"+opt.dataset+"_epoch_"+str(epoch+1)+"_backbone_"+str(opt.depth)+".pth"))
+
+            # Early stopping
+            if epoch - best_epoch > opt.es_patience > 0:
+                print("Stop training at epoch {}. The lowest loss achieved is {}".format(epoch, loss))
+                break
+    writer.close()
+
+
+    #     torch.save(retinanet.module, 'trained_models/{}_retinanet_{}.pt'.format(opt.dataset, epoch_num))
+
+    # retinanet.eval()
+
+    # torch.save(retinanet, 'trained_models/model_final.pt')
 
 
 if __name__ == '__main__':
